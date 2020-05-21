8098ef4ee576c95c372ad4b22febfe18d3befdda
Aleksandr Rogembaum <rogembaum@gmail.com>
2020-05-21 17:07:56 +0700

    add_proxy&deploys.sh

---
+++ identidock/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+echo "Starting identidock system"
+docker run -d --restart=always --name redis redis:3
+docker run -d --restart=always --name dnmonster amouat/dnmonster:1.0
+docker run -d --restart=always --link dnmonster:dnmonster --link redis:redis \
+-e ENV=PROD --name identidock rogembaum/identidock:newest
+docker run -d --restart=always --name proxy --link identidock:identidock -p 80:80 \
+-e NGINX_HOST=192.168.112.117 -e NGINX_PROXY=http://identidock:9090 \
+rogembaum/proxy:1.0
+echo "Started"
